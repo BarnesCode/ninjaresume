e4b938419e43564276b4d19a567dfbe18ae6cfaa
Justin Barnes <justin@embarke.com>
2014-10-20 15:38:10 -0700

    get tagcloud working

--- app.js
+++ app.js
@@ -11,13 +11,35 @@
                       cont.skills = data.skills;
                       cont.accomplishments = data.accomplishments;
                       cont.jobskills = data.jobskills;
-                      cont.bob = data.bob;
+                      setTimeout(function () { $("#skills a").tagcloud() }, 10);
+                      //setTimeout(function () { $('body').scrollspy('refresh'); }, 10);
                   })
                   .error(function () {
                       //alert('error');
                   });
     }]);
+    //app.directive('tagcloud', function () {
+    //    return {
+    //        restrict: 'E',
+    //        replace: true,
+    //        scope: {
+
+    //        },
+    //        templateUrl: 'tagcloud.html',
+    //        //link: function (scope, element) {
+    //        //    printObject(scope.$root.resume);
+    //        //}
+    //    };
+    //});
     app.run(function () {
         $('body').scrollspy('refresh');
     });
 })();
+
+function printObject(o) {
+    var out = '';
+    for (var p in o) {
+        out += p + ': ' + o[p] + '\n';
+    }
+    alert(out);
+}
